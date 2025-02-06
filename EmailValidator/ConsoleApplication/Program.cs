fcfcb0b914d47b6e9d1efbbd73359976f629c9e8
pardeep singh <pardeep@vineforce.net>
2025-02-06 15:25:03 +0530

    Create a disposable email class library project and console app

---
+++ EmailValidator/ConsoleApplication/Program.cs
@@ -0,0 +1,33 @@
+﻿using Vineforce;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the email address to check if it's disposable:");
+            string userEmail = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(userEmail))
+            {
+                // Call the method to check if the email is disposable
+                EmailValidator emailValidator = new EmailValidator();
+                bool isDisposable = emailValidator.IsDisposableEmail(userEmail);
+                // Output the result
+                if (isDisposable)
+                {
+                    Console.WriteLine($"The email '{userEmail}' is disposable.");
+                }
+                else
+                {
+                    Console.WriteLine($"The email '{userEmail}' is not disposable.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid email address.");
+            }
+        }
+    }
+}
---
+++ EmailValidator/EmailValidator/EmailValidator.cs
@@ -0,0 +1,21 @@
+﻿namespace Vineforce
+{
+    public class EmailValidator
+    {
+        #region IsDisposableEmail
+        /// <summary>
+        ///  IsDisposableEmail 
+        /// </summary>
+        /// <param name="Email"></param>
+        /// <returns>Bool</returns>
+        public bool IsDisposableEmail(string Email)
+        {
+            var domain = Email.Split('@')[1].ToLower();
+            List<string> emailList = DisposableEmailDomains.DisposabledEmail;
+            var isExist = emailList.Contains(domain);
+            if (isExist) return true;
+            return false;
+        }
+        #endregion
+    }
+}
