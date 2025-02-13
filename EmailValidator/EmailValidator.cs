60be83d396c8b206a59bd293a31f56c8b023e7a4
pardeep singh <pardeep@vineforce.net>
2025-02-13 15:22:22 +0530

    AB#7696 change in email validator file

--- EmailValidator/EmailValidator.cs
+++ EmailValidator/EmailValidator.cs
@@ -1,4 +1,6 @@
-﻿namespace Vineforce
+﻿using System.Text.RegularExpressions;
+
+namespace Vineforce
 {
     public class EmailValidator
     {
@@ -10,10 +12,35 @@
         /// <returns>Bool</returns>
         public bool IsDisposableEmail(string Email)
         {
-            var domain = Email.Split('@')[1].ToLower();
-            var isExist = DisposableEmailDomains.DisposabledEmail.Contains(domain);
-            if (isExist) return true;
-            return false;
+            try
+            {
+                if (!IsValidEmail(Email))
+                {
+                    throw new ArgumentException("The provided email address is invalid.");
+                }
+                var domain = Email.Split('@')[1].ToLower();
+                var isExist = DisposableEmailDomains.DisposabledEmail.Contains(domain);
+                if (isExist) return true;
+                return false;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            
+        }
+
+        /// <summary>
+        ///  Validate the email enter by user
+        /// </summary>
+        /// <param name="email">Enter your email address </param>
+        /// <returns>Bool</returns>
+        private static bool IsValidEmail(string email)
+        {
+            string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            Regex regex = new Regex(pattern);
+            return regex.IsMatch(email);
         }
         #endregion
     }
